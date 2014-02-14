5753398ab936a570ee1b4879e5b70c9809ad3ba6
Stefan Fussenegger <stf+git@molindo.at>
2014-02-14 13:19:49 +0100

    support Maven versioning scheme (StaticMavenArtifactVersionProvider)

---
+++ src/main/java/org/wicketstuff/mergedresources/versioning/MavenResourceVersion.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2010 Molindo GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wicketstuff.mergedresources.versioning;
+
+import org.wicketstuff.mergedresources.thirdparty.org.apache.maven.artifact.versioning.ArtifactVersion;
+
+public final class MavenResourceVersion extends AbstractResourceVersion {
+
+	private static final long serialVersionUID = 1L;
+
+	private final ArtifactVersion _version;
+
+	public MavenResourceVersion(ArtifactVersion version) {
+		if (version == null) {
+			throw new NullPointerException("version");
+		}
+		_version = version;
+	}
+
+	@Override
+	public boolean isValid() {
+		return true;
+	}
+
+	@Override
+	public String getVersion() {
+		return _version.toString();
+	}
+
+	public ArtifactVersion getArtifactVersion() {
+		return _version;
+	}
+
+	@Override
+	protected int compareValid(AbstractResourceVersion o) throws IncompatibleVersionsException {
+		if (o instanceof MavenResourceVersion) {
+			return getArtifactVersion().compareTo(((MavenResourceVersion) o).getArtifactVersion());
+		} else {
+			throw new IncompatibleVersionsException(this, o);
+		}
+	}
+}
---
+++ src/main/java/org/wicketstuff/mergedresources/versioning/StaticMavenArtifactVersionProvider.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2010 Molindo GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wicketstuff.mergedresources.versioning;
+
+import org.wicketstuff.mergedresources.thirdparty.org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.wicketstuff.mergedresources.thirdparty.org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+
+public class StaticMavenArtifactVersionProvider implements IResourceVersionProvider {
+
+	private final MavenResourceVersion _version;
+
+	public StaticMavenArtifactVersionProvider(String version) {
+		this(new DefaultArtifactVersion(version));
+	}
+
+	public StaticMavenArtifactVersionProvider(ArtifactVersion version) {
+		if (version == null) {
+			throw new NullPointerException("version");
+		}
+		_version = new MavenResourceVersion(version);
+	}
+
+	@Override
+	public AbstractResourceVersion getVersion(Class<?> scope, String file) throws VersionException {
+		return _version;
+	}
+
+}
