d2f8300544f870d985792dbc5c6f677a1fb0bfdb
msparer <msp+git@molindo.at>
2014-11-14 10:57:02 +0100

    went back to yuicompressor version 2.4.6 due to a bug that falsely replaces "1.0em" with "1.0" (in an attempt to replace obsolete 0em with 0)

---
+++ src/test/java/org/wicketstuff/mergedresources/util/YuiCompressorUtilTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2014 Molindo GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wicketstuff.mergedresources.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class YuiCompressorUtilTest {
+
+	@Test
+	public void testCompressZeroReplacement() {
+
+		// yui should replace 0em with 0
+		{
+			String toCompress = ".fooClass {\n\tfont-size: 0em;\n}\n";
+
+			// also strips whitespace and last semicolon
+			assertEquals(".fooClass{font-size:0}", YuiCompressorUtil.compress(toCompress));
+		}
+
+		// it should work however if it's 1.0em
+		{
+			String toCompress = ".fooClass {\n\tfont-size: 1.0em;\n}\n";
+
+			// also strips whitespace and last semicolon
+			assertEquals(".fooClass{font-size:1.0em}", YuiCompressorUtil.compress(toCompress));
+		}
+	}
+
+}
